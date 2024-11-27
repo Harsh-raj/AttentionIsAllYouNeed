55730a6531edddab6abe5e4bfc0f475b0fac6fdc
Harsh-raj <rajharsh209@gmail.com>
2024-11-28 01:50:38 +0530

    Implementation of Projection layer for transformer model.

---
+++ models.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+class ProjectionLayer(nn.Module):
+  
+  def __init__(self, d_model: int, vocab_size: int) -> None:
+    super().__init__()
+    self.proj = nn.Linear(d_model, vocab_size)
+    
+  def forward(self, x):
+    # (batch, seq_len, d_model) --> (batch, seq_len, vocab_size)
+    
+    return torch.log_softmax(self.proj(x), dim = -1)
