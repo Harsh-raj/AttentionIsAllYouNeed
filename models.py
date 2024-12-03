5fdb082fff8c887a02b6634c693b2672877185e1
Harsh-raj <rajharsh209@gmail.com>
2024-12-04 02:36:08 +0530

    added features to residual connections in Decoder block

--- models.py
+++ models.py
@@ -3,12 +3,12 @@ import torch.nn as nn
 
 class DecoderBlock(nn.Module):
   
-  def __init__(self, self_attention_block: MultiHeadAttentionBlock, cross_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float):
+  def __init__(self, features: int, self_attention_block: MultiHeadAttentionBlock, cross_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float):
     super().__init__()
     self.self_attention_block = self_attention_block
     self.cross_attention_block = cross_attention_block
     self.feed_forward_block = feed_forward_block
-    self.residual_connection = nn.ModuleList([ResidualConnection(dropout) for _ in range(3)])
+    self.residual_connection = nn.ModuleList([ResidualConnection(features, dropout) for _ in range(3)])
     
   def forward(self, x, encoder_output, src_mask, target_mask):
     x = self.residual_connection[0](x, lambda x: self.self_attention_block(x, x, x, target_mask))
