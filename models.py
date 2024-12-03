e161b8871513d531a392fc979ef446db62e30c45
Harsh-raj <rajharsh209@gmail.com>
2024-12-04 03:13:15 +0530

    modified the build_transformer class at various positions.

--- models.py
+++ models.py
@@ -14,10 +14,10 @@ class BuildTransformer:
     # Create the encoder blocks
     encoder_blocks = []
     for _ in range(N):
-      encoder_self_attention_blocks = MultiHeadAttentionBlock(d_model, h, dropout)
+      encoder_self_attention_block = MultiHeadAttentionBlock(d_model, h, dropout)
       feed_forward_block = FeedForwardBlock(d_model, d_ff, dropout)
       encoder_block = EncoderBlock(encoder_self_attention_blocks, feed_forward_block, dropout)
-      encoder_block.append(encoder_block)
+      encoder_blocks.append(encoder_block)
       
     # Create the decoder blocks
     decoder_blocks = []
@@ -25,18 +25,18 @@ class BuildTransformer:
       decoder_self_attention_block = MultiHeadAttentionBlock(d_model, h, dropout)
       decoder_cross_attention_block = MultiHeadAttentionBlock(d_model, h, dropout)
       feed_forward_block = FeedForwardBlock(d_model, d_ff, dropout)
-      decoder_blocks = decoderBlock(decoder_self_attention_block, decoder_cross_attention_block, feed_forward_block, dropout)
-      decoder_blocks.append(decoder_blocks)
+      decoder_block = DecoderBlock(d_model, decoder_self_attention_block, decoder_cross_attention_block, feed_forward_block, dropout)
+      decoder_blocks.append(decoder_block)
       
     # Create the encoder and the decoder
-    encoder = Encoder(nn.ModuleList(encoder_blocks))
-    decoder = Decoder(nn.ModuleList(decoder_blocks))
+    encoder = Encoder(d_model, nn.ModuleList(encoder_blocks))
+    decoder = Decoder(d_model, nn.ModuleList(decoder_blocks))
     
     # Create the projection layer
-    projection_layer = projectionLayer(d_model, target_vocab_size)
+    projection_layer = ProjectionLayer(d_model, target_vocab_size)
     
     # Create the transformer
-    transformer = transformer(encoder, decoder, source_embed, target_embed, source_pos, target_pos, projection_layer)
+    transformer = Transformer(encoder, decoder, source_embed, target_embed, source_pos, target_pos, projection_layer)
     
     # Initialize the parameters
     for p in transformer.parameters():
