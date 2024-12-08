ce3ae30be2f261a35e9a0e1a519d878cfac120cc
Harsh-raj <rajharsh209@gmail.com>
2024-12-08 11:14:49 +0530

    Changed batch size as colab instance

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ class Config:
   @staticmethod
   def get_config():
     return {
-        "batch_size": 8,
+        "batch_size": 1,
         "num_epochs": 10,
         "lr": 10**-4,
-        "seq_len": 200
+        "seq_len": 350
         "d_model": 512,
         "datasource": 'opus_books',
         "lang_src": "en",
