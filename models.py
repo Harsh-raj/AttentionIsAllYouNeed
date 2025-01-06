e56c380497924e469b84cd140f7b787f5caf7714
Harsh-raj <rajharsh209@gmail.com>
2025-01-06 20:03:14 +0530

    modified code with working one

--- models.py
+++ models.py
@@ -8,8 +8,13 @@ class LayerNormalization(nn.Module):
     
     self.alpha = nn.Parameter(torch.ones(1)) #Multiplied
     self.bias = nn.Parameter(torch.zeros(1)) #Added
+    #alpha and bias are learnable parameters
     
   def forward(self, x):
-    mean = x.mean(dim = -1, keepdim=True)
-    std = x.std(dim = -1, keepdim = True)
+    # x: (batch, seq_len, hidden_size)
+    # Keep the dimension for broadcasting
+    mean = x.mean(dim = -1, keepdim=True)# (batch, seq_len, 1)
+    # Keep the dimension for broadcasting
+    std = x.std(dim = -1, keepdim = True)# (batch, seq_len, 1)
+    # eps is to prevent dividing by zero or when std is very small
     return self.alpha * (x - mean) / (std + self.eps) + self.bias
