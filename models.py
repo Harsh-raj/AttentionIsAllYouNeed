47416927be6b1bcc522eb21de6662d0c177a8c96
Harsh-raj <rajharsh209@gmail.com>
2025-01-06 19:47:07 +0530

    modified code with working one

--- models.py
+++ models.py
@@ -6,9 +6,9 @@ class Decoder(nn.Module):
   def __init__(self, features: int, layers: nn.ModuleList) -> None:
     super().__init__()
     self.layers = layers
-    self.norms = LayerNormalization()
+    self.norms = LayerNormalization(features)
     
-  def forward(self, x, encoder_output, src_mask, target_mask):
+  def forward(self, x, encoder_output, src_mask, tgt_mask):
     for layer in self.layers:
-      x = layer(x, encoder_output, src_mask, target_mask)
+      x = layer(x, encoder_output, src_mask, tgt_mask)
     return self.norm(x)
