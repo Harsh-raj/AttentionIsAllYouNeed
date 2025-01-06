b2c400d0440525374e1432a5519671753367d578
Harsh-raj <rajharsh209@gmail.com>
2025-01-06 21:39:17 +0530

    added comments

--- model.py
+++ model.py
@@ -1,17 +1,6 @@
 import torch
 import torch.nn as nn
 import math
-
-class InputEmbeddings(nn.Module):
-  
-  def __init__(self, d_model: int, vocab_size: int):
-    super().__init__()
-    self.d_model = d_model
-    self.vocab_size = vocab_size
-    self.embedding = nn.embedding(vocab_size, d_model)
-    
-  def forward(self, x):
-    return self.embedding(x) * math.sqrt(self.d_model)
   
 class PositionalEncoding(nn.Module):
   
@@ -25,16 +14,20 @@ class PositionalEncoding(nn.Module):
     pe = torch.zeros(seq_len, d_model)
     
     #create a vector of shape (seq_len, 1)
-    position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
-    div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0)/d_model))
+    position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1) # (seq_len, 1)
+    
+    # Create a vector of shape (d_model, 1)
+    div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0)/d_model)) # (d_model / 2)
     #apply sine to even positions
-    pe[:, 0::2] = torch.sin(position * div_term)
-    pe[:, 1::2] = torch.cos(position * div_term)
+    pe[:, 0::2] = torch.sin(position * div_term)  # sin(position * (10000 ** (2i / d_model))
+    # Apply cosine to odd indices
+    pe[:, 1::2] = torch.cos(position * div_term)  # cos(position * (10000 ** (2i / d_model))
     
+    # Add a batch dimension to the positional encoding
     pe = pe.unsqueeze(0) # (1, seq_len, d_model)
-    
+    # Register the positional encoding as a buffer
     self.register_buffer('pe', pe)
 
   def forward(self, x):
-    x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)
+    x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)  # (batch, seq_len, d_model)
     return self.dropout(x)
