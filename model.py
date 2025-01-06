4e5d0a2a9a3c4d0167b85e7f7064a44d3fdc7467
Harsh-raj <rajharsh209@gmail.com>
2025-01-06 20:24:22 +0530

    Modified code with working one

--- model.py
+++ model.py
@@ -6,11 +6,12 @@ class MultiHeadAttentionBlock(nn.Module):
 
   def __init__(self, d_model: int, h: int, dropout: float) -> None:
     super().__init__()
-    self.d_model = d_model
-    self.h = h
+    self.d_model = d_model # Embedding vector size
+    self.h = h #Number of heads
+    # Make sure d_model is divisible by h
     assert d_model % h == 0, "d_model is not divisible by h"
     
-    self.d_k = d_model // h
+    self.d_k = d_model // h # Dimension of vector seen by each other head
     self.w_q = nn.Linear(d_model, d_model, bias=False) #wq
     self.w_k = nn.Linear(d_model, d_model, bias=False) #wk
     self.w_v = nn.Linear(d_model, d_model, bias=False) #wv
@@ -21,17 +22,19 @@ class MultiHeadAttentionBlock(nn.Module):
   @staticmethod
   def attention(query, key, value, mask, dropout: nn.Dropout):
     d_k = query.shape[-1]
-    
+    # Just apply the formula from the paper
     # (batch, h, seq_len, d_k) --> (batch, h, seq_len, seq_len)
     attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
     if mask is not None:
+      # Write a very low value (indicating -inf) to the positions where mask == 0
       attention_scores.masked_fill_(mask == 0, -1e9)
     
-    attention_scores = attention_scores.softmax(dim = -1) # (batch, h, seq_len, seq_len)
+    attention_scores = attention_scores.softmax(dim = -1) # (batch, h, seq_len, seq_len) Apply softmax
     
     if dropout is not None:
       attention_scores = dropout(attention_scores)
-      
+    # (batch, h, seq_len, seq_len) --> (batch, h, seq_len, d_k)
+    # return attention scores which can be used for visualization  
     return (attention_scores @ value), attention_scores
     
      
@@ -45,11 +48,14 @@ class MultiHeadAttentionBlock(nn.Module):
     key = query.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
     value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2)
     
+    # Calculate attention
     x, self.attention_scores = MultiHeadAttentionBlock.attention(query, key, value, mask, self.dropout)
     
+    # Combine all the heads together
     # (batch, h, seq_len, d_k) --> (batch, seq_len, h, d_k) --> (batch, seq_len, d_model)
     x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
     
+    # Multiply by Wo
     # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
     return self.w_o(x)
     
